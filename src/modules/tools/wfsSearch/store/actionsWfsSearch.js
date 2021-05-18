1c1d63e6f8b676d36d6544ee83972b71ee54f140
Pascal Röhling <pascal.roehling@dataport.de>
2021-05-18 21:54:25 +0200

    LGVP1-81: Added JSDoc for the actions.

--- src/modules/tools/wfsSearch/store/actionsWfsSearch.js
+++ src/modules/tools/wfsSearch/store/actionsWfsSearch.js
@@ -3,12 +3,23 @@ import handleAxiosResponse from "../../../../utils/handleAxiosResponse";
 import {setLikeFilterProperties} from "../utils/buildFilter";
 import {prepareLiterals} from "../utils/literalFunctions";
 
-// TODO: JSDoc
 const actions = {
-    instanceChanged ({commit, dispatch}, instanceId) {
-        commit("setCurrentInstanceId", instanceId);
+    /**
+     * Updates the currently set search instance based on the given id and prepares the tool for that instance.
+     *
+     * @param {Number} instanceIndex The index of the currentInstance.
+     * @returns {void}
+     */
+    instanceChanged ({commit, dispatch}, instanceIndex) {
+        commit("setCurrentInstanceIndex", instanceIndex);
         dispatch("prepareModule");
     },
+    /**
+     * If the WFS is given, the required values are set, information from the external source (dropdowns) is retrieved,
+     * like filter values and the service are set.
+     *
+     * @returns {void}
+     */
     prepareModule ({commit, dispatch, getters}) {
         dispatch("resetModule", false);
 
@@ -41,18 +52,30 @@ const actions = {
             dispatch("Alerting/addSingleAlert", i18next.t("common:modules.tools.wfsSearch.wrongConfig", {name: this.name}), {root: true});
         }
     },
-    resetModule ({commit}, closedTool) {
+    /**
+     * Resets state parameters to its initial state.
+     * If the tool was closed, the tool is completely reset.
+     *
+     * @param {Boolean} closeTool Whether the tool was closed or not.
+     * @returns {void}
+     */
+    resetModule ({commit}, closeTool) {
         commit("setAddedOptions", []);
         commit("setRequiredValues", null);
         commit("setSelectedOptions", {});
         commit("setService", null);
 
-        if (closedTool) {
+        if (closeTool) {
             commit("setCurrentInstance", 0);
             commit("setParsedSource", null);
             commit("setActive", false);
         }
     },
+    /**
+     * Retrieves information from the external file to the state.
+     *
+     * @returns {void}
+     */
     retrieveData ({commit, getters}) {
         const {currentInstance: {selectSource}} = getters;
 
