f7334bf846009e01d3911d33e52698ce7d3d02c5
Pascal Röhling <pascal.roehling@dataport.de>
2021-05-18 22:12:21 +0200

    LGVP1-81: Added JSDoc for getters.

--- src/modules/tools/wfsSearch/store/gettersWfsSearch.js
+++ src/modules/tools/wfsSearch/store/gettersWfsSearch.js
@@ -1,14 +1,26 @@
 import {generateSimpleGetters} from "../../../../app-store/utils/generators";
 import initialState from "./stateWfsSearch";
 
-// TODO: JSDoc
 const getters = {
     ...generateSimpleGetters(initialState),
+    /**
+     * Tests whether all required values contain a value.
+     *
+     * @param {Object} requiredValues The object containing the values which should be set before starting the search.
+     * @returns {Boolean} Whether all required values have been set or not.
+     */
     requiredFields ({requiredValues}) {
         return !Object.values(requiredValues).every(val => typeof val === "string" && val !== "");
     },
-    currentInstance ({instances, currentInstanceId}) {
-        return instances[currentInstanceId];
+    /**
+     * Returns the currently selected searchInstance.
+     *
+     * @param {Object[]} instances Array of searchInstances.
+     * @param {Number} currentInstanceIndex Index of the current searchInstance inside the array of instances.
+     * @returns {Object} The current searchInstance.
+     */
+    currentInstance ({instances, currentInstanceIndex}) {
+        return instances[currentInstanceIndex];
     }
 };
 
