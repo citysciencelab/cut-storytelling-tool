b80fef64d36e5f42e92d4cb2eee1ff6fdcfc58cf
Darge <DargeFi@fhhnet.stadt.hamburg.de>
2021-06-08 07:21:29 +0200

    LGVP1-95 added missing util file for axios response

---
+++ src/utils/handleAxiosResponse.js
@@ -0,0 +1,26 @@
+/**
+ * Returns the data from the axios response.
+ *
+ * @throws Will throw an error if the response is not valid.
+ * @param {Object} response The response received by axios.
+ * @param {String} callContext Name of the outer function and Component (if given) which used axios.
+ * @returns {Object} The received data or undefined if an error occurred.
+ */
+export default function handleAxiosResponse (response, callContext) {
+    if (
+        response === null
+        || typeof response !== "object"
+        || !response.hasOwnProperty("status")
+        || !response.hasOwnProperty("statusText")
+        || !response.hasOwnProperty("data")
+    ) {
+        console.warn(`${callContext}, handleAxiosResponse: response`, response);
+        throw Error(`${callContext}, handleAxiosResponse: The received response is not valid.`);
+    }
+    else if (response.status !== 200) {
+        console.warn(`${callContext}, handleAxiosResponse: response`, response);
+        throw Error(`${callContext}, handleAxiosResponse: The received status code indicates an error.`);
+    }
+
+    return response.data;
+}
