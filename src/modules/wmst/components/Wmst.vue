8d54aa1e94ed16567ae0de64dffb6de8d31756c1
Pascal Röhling <pascal.roehling@dataport.de>
2021-06-14 12:02:49 +0200

    LGVP1-96: Second TimeSlider window is now only displayed if the size of the screen is big enough (> 800px);

--- src/modules/wmst/components/Wmst.vue
+++ src/modules/wmst/components/Wmst.vue
@@ -7,38 +7,61 @@ export default {
     components: {
         TimeSlider
     },
+    data: () => ({width: window.innerWidth, mobileWidth: 800}),
     computed: {
-        ...mapGetters("Wmst", ["swiper", "timeSlider"])
+        ...mapGetters("Wmst", ["swiper", "timeSlider"]),
+        minWidth () {
+            return this.width > this.mobileWidth;
+        }
+    },
+    created () {
+        window.addEventListener("resize", this.innerWidthChanged);
+    },
+    methods: {
+        innerWidthChanged () {
+            this.width = window.innerWidth;
+        }
     }
 };
 </script>
 
 <template>
-    <div>
+    <div id="wmst">
         <TimeSlider
             v-if="timeSlider.active"
-            :class="{'moveLeft': swiper.active}"
+            :class="{'moveLeft': swiper.active && minWidth}"
+            :layer-id="timeSlider.layerId"
         />
-        <!-- TODO: Make sure that the correct layer will be displayed after deselection! -->
         <TimeSlider
-            v-if="timeSlider.active && swiper.active"
+            v-if="timeSlider.active && swiper.active && minWidth"
             :class="{'moveRight': swiper.active}"
+            :layer-id="timeSlider.layerId + '_secondLayer'"
         />
-        <!-- <LayerSwiper v-if="btnClicked" />-->
+        <!-- <LayerSwiper v-if="swiper.active && minWidth" />-->
     </div>
 </template>
 
 <style lang="less" scoped>
     .transform(@value) {
         transform: translateX(@value);
+        transition: ease transform 250ms;
     }
     .timeSlider-wrapper {
         .transform(-50%);
     }
     .moveLeft {
-        .transform(-120%);
+        .transform(-110%);
     }
     .moveRight {
-        .transform(20%);
+        .transform(10%);
+    }
+
+    @media (min-width: 1075px) {
+        .moveLeft {
+            .transform(-150%);
+        }
+        .moveRight {
+            .transform(50%);
+        }
     }
 </style>
