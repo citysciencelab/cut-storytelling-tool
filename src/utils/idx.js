ad3c6c411f91dc2fd160e1faa2218845f01e79aa
Pascal Röhling <pascal.roehling@dataport.de>
2021-06-29 22:18:16 +0200

    LGVP1-81: Added unit tests for the utility function 'idx'.

--- src/utils/idx.js
+++ src/utils/idx.js
@@ -3,8 +3,8 @@
  * to retrieve data.
  * Copied from https://medium.com/javascript-inside/safely-accessing-deeply-nested-values-in-javascript-99bf72a0855a.
  *
- * @param {Object} object The object to traverse.
- * @param {String[]} path The path of keys / indices to traverse through the object.
+ * @param {object} object The object to traverse.
+ * @param {string[]} path The path of keys / indices to traverse through the object.
  * @returns {?*} The value(s) to be retrieved from the given object.
  */
 export default (object, path) => path.reduce(
