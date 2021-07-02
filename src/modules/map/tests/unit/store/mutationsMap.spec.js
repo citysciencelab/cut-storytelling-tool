9e8e826b28188bb48142375361ccfe0f974d0439
Pascal Röhling <pascal.roehling@dataport.de>
2021-07-02 22:14:51 +0200

    LGVP1-81: Fixed the map tests.

--- src/modules/map/tests/unit/store/mutationsMap.spec.js
+++ src/modules/map/tests/unit/store/mutationsMap.spec.js
@@ -1,111 +1,12 @@
 import {expect} from "chai";
-import sinon from "sinon";
 import mutations from "../../../store/mutationsMap";
 import Map from "ol/Map";
 import VectorLayer from "ol/layer/Vector.js";
 import VectorSource from "ol/source/Vector.js";
 
-const {setCenter, addLayerToMap, removeLayerFromMap} = mutations;
+const {addLayerToMap, removeLayerFromMap} = mutations;
 
 describe("src/modules/map/store/mutationsMap.js", () => {
-    describe("setCenter", () => {
-        it("should set the center", () => {
-            const state = {
-                center: [0, 7],
-                map: {
-                    getView: function () {
-                        return {
-                            setCenter: sinon.spy()
-                        };
-                    }
-                }
-            };
-
-            setCenter(state, [3, 5]);
-            expect(state.center).to.deep.equal([3, 5]);
-        });
-
-        it("should not set the center, if the coordinate (['3', 5]) has the wrong data type", () => {
-            const state = {
-                center: [0, 7],
-                map: {
-                    getView: function () {
-                        return {
-                            setCenter: sinon.spy()
-                        };
-                    }
-                }
-            };
-
-            setCenter(state, ["3", 5]);
-            expect(state.center).to.deep.equal([0, 7]);
-        });
-
-        it("should not set the center, if the coordinate ([3, '5']) has the wrong data type", () => {
-            const state = {
-                center: [0, 7],
-                map: {
-                    getView: function () {
-                        return {
-                            setCenter: sinon.spy()
-                        };
-                    }
-                }
-            };
-
-            setCenter(state, [3, "5"]);
-            expect(state.center).to.deep.equal([0, 7]);
-        });
-
-        it("should not set the center, if the coordinate is not an array", () => {
-            const state = {
-                center: [0, 7],
-                map: {
-                    getView: function () {
-                        return {
-                            setCenter: sinon.spy()
-                        };
-                    }
-                }
-            };
-
-            setCenter(state, {3: "5"});
-            expect(state.center).to.deep.equal([0, 7]);
-        });
-
-        it("should not set the center, if the length of the coordinate is greater than two", () => {
-            const state = {
-                center: [0, 7],
-                map: {
-                    getView: function () {
-                        return {
-                            setCenter: sinon.spy()
-                        };
-                    }
-                }
-            };
-
-            setCenter(state, [0, 3, 3]);
-            expect(state.center).to.deep.equal([0, 7]);
-        });
-
-        it("should not set the center, if the length of the coordinate is lower than two", () => {
-            const state = {
-                center: [0, 7],
-                map: {
-                    getView: function () {
-                        return {
-                            setCenter: sinon.spy()
-                        };
-                    }
-                }
-            };
-
-            setCenter(state, [8]);
-            expect(state.center).to.deep.equal([0, 7]);
-        });
-    });
-
     describe("addLayerToMap", () => {
         it("should add a layer to the map", () => {
             const state = {
