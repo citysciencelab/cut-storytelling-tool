58ffc2b2a395429998b046016505ab9dba099568
Pascal Röhling <pascal.roehling@dataport.de>
2021-07-02 15:47:00 +0200

    LGVP1-81: Fixed 'zoomTo' action of the Map so that it properly accepts options and morphed the mutation 'setCenter' to an action so that 'zoomTo' can use it as well.

--- src/modules/map/store/mutationsMap.js
+++ src/modules/map/store/mutationsMap.js
@@ -6,21 +6,6 @@ import BaseLayer from "ol/layer/Base";
 const mutations = {
     ...generateSimpleMutations(initialState),
     /**
-     * Set the center of the current view.
-     * @param {Object} state - the map state
-     * @param {Number[]} coord - an array of numbers representing an xy coordinate
-     * @returns {void}
-     */
-    setCenter (state, coord) {
-        if (Array.isArray(coord) && coord.length === 2 && typeof coord[0] === "number" && typeof coord[1] === "number") {
-            state.center = coord;
-            state.map.getView().setCenter(coord);
-        }
-        else {
-            console.warn("Center was not set. Probably there is a data type error. The format of the coordinate must be an array with two numbers.");
-        }
-    },
-    /**
      * Sets the visibility of a layer.
      * @param {Object} state state object
      * @param {Object} payload parameter object
