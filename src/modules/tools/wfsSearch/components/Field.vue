e3f0b227597f0a0b0306543b0d7c26d841d96697
Pascal Röhling <pascal.roehling@dataport.de>
2021-07-02 11:42:52 +0200

    LGVP1-81: Utilized utility function 'isObject' instead of the direct function call.

--- src/modules/tools/wfsSearch/components/Field.vue
+++ src/modules/tools/wfsSearch/components/Field.vue
@@ -3,6 +3,7 @@ import {mapActions, mapGetters, mapMutations} from "vuex";
 import actions from "../store/actionsWfsSearch";
 import getters from "../store/gettersWfsSearch";
 import mutations from "../store/mutationsWfsSearch";
+import isObject from "../../../../utils/isObject";
 import {buildXmlFilter} from "../utils/buildFilter";
 import {fieldValueChanged} from "../utils/literalFunctions";
 import {buildPath, getOptions, prepareOptionsWithId} from "../utils/pathFunctions";
@@ -84,7 +85,7 @@ export default {
                     dropdownInputUsesId: this.multipleValues(this.dropdownInputUsesId),
                     inputPlaceholder: this.multipleValues(this.inputPlaceholder),
                     inputTitle: this.multipleValues(this.inputTitle),
-                    options: Array.isArray(this.options) && Object.prototype.toString.call(this.options[0]) !== "[object Object]" ? this.options[this.parameterIndex] : this.options,
+                    options: Array.isArray(this.options) && isObject(this.options[0]) ? this.options[this.parameterIndex] : this.options,
                     required: this.multipleValues(this.required),
                     suggestionsConfig: this.multipleValues(this.suggestionsConfig),
                     type: this.multipleValues(this.type)
