bde2e79fc4c25ebea46e0d4636ae0a7a2e9bd232
Pascal Röhling <pascal.roehling@dataport.de>
2021-07-23 16:19:19 +0200

    LGVP1-81: Added tests for the utility function 'prepareOptionsWithId' of the tool wfsSearch.

--- src/modules/tools/wfsSearch/tests/utils/pathFunctions.spec.js
+++ src/modules/tools/wfsSearch/tests/utils/pathFunctions.spec.js
@@ -1,7 +1,50 @@
 import {expect} from "chai";
-import {removePath} from "../../utils/pathFunctions";
+import {prepareOptionsWithId, removePath} from "../../utils/pathFunctions";
 
 describe("src/modules/tools/wfsSearch/utils/pathFunctions.js", () => {
+    describe("prepareOptionsWithId", () => {
+        it("should return an array of option-objects containing the parameter 'fieldValue' and 'displayName'", () => {
+            const ids = ["741", "879", "2038", "3954"],
+                elements = [
+                    {
+                        id: ids[0],
+                        nenner: ["0", "420"]
+                    },
+                    {
+                        id: ids[1],
+                        nenner: ["420"]
+                    },
+                    {
+                        id: ids[2],
+                        nenner: ["420"]
+                    },
+                    {
+                        id: ids[3],
+                        nenner: ["420"]
+                    }
+                ];
+
+            expect(prepareOptionsWithId(elements)).to.eql([
+                {
+                    fieldValue: ids[0],
+                    displayName: ids[0]
+                },
+                {
+                    fieldValue: ids[1],
+                    displayName: ids[1]
+                },
+                {
+                    fieldValue: ids[2],
+                    displayName: ids[2]
+                },
+                {
+                    fieldValue: ids[3],
+                    displayName: ids[3]
+                }
+            ]);
+        });
+    });
+
     describe("removePath", () => {
         const element = "val";
 
