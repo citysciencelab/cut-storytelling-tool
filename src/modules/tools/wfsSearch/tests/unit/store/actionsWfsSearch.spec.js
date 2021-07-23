cda032fc827209423e62ba17c25348e09a9582aa
Pascal Röhling <pascal.roehling@dataport.de>
2021-07-23 15:26:30 +0200

    LGVP1-81: Removed describe.only.

--- src/modules/tools/wfsSearch/tests/unit/store/actionsWfsSearch.spec.js
+++ src/modules/tools/wfsSearch/tests/unit/store/actionsWfsSearch.spec.js
@@ -3,7 +3,7 @@ import {expect} from "chai";
 import actions from "../../../store/actionsWfsSearch";
 import isObject from "../../../../../../utils/isObject";
 
-describe.only("src/modules/tools/wfsSearch/store/actionsWfsSearch.js", () => {
+describe("src/modules/tools/wfsSearch/store/actionsWfsSearch.js", () => {
     let commit, dispatch;
 
     beforeEach(() => {
