beca0ffcc8d8b27d1d7b00a8696af4322ae9c3d6
sven_beyel <sven.beyel@gv.hamburg.de>
2021-07-29 08:39:33 +0200

    update set config.json and config.js in portal master back to dev state
    BG-1769

--- portal/master/config.js
+++ portal/master/config.js
@@ -33,7 +33,6 @@ const Config = {
         ["EPSG:31467", "+title=Bessel/Gauß-Krüger 3 +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
         // ETRS89 UTM
         ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"],
-        ["EPSG:25833", "+title=EPSG:25833 +proj=utm +zone=33 +ellps=WGS84 +towgs84=0,0,0,0,0,0,1 +units=m +no_defs"],
         // LS 320: zusammen mit Detlef Koch eingepflegt und geprüft
         ["EPSG:8395", "+title=ETRS89/Gauß-Krüger 3 +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=GRS80 +datum=GRS80 +units=m +no_defs"],
         // WGS84
