a11178621c6a1a0d6dbc5dd120e777916e6110ac
Florian Schlage <schlage@traffgoroad.com>
2021-10-01 08:47:01 +0200

    tests RoutingSpeedProfileIcon.vue

--- src/modules/tools/routing/components/RoutingSpeedProfileIcon.vue
+++ src/modules/tools/routing/components/RoutingSpeedProfileIcon.vue
@@ -31,6 +31,7 @@ export default {
 
         <template v-if="speedProfileId === 'CAR'">
             <path
+                class="routing-speed-profile-icon-CAR"
                 d="M0 0h24v24H0V0z"
                 fill="none"
             /><path d="M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.85 7h10.29l1.08 3.11H5.77L6.85 7zM19 17H5v-5h14v5z" /><circle
@@ -46,6 +47,7 @@ export default {
 
         <template v-if="speedProfileId === 'HGV'">
             <path
+                class="routing-speed-profile-icon-HGV"
                 d="M0 0h24v24H0V0z"
                 fill="none"
             /><path d="M20 8h-3V4H3c-1.1 0-2 .9-2 2v11h2c0 1.66 1.34 3 3 3s3-1.34 3-3h6c0 1.66 1.34 3 3 3s3-1.34 3-3h2v-5l-3-4zm-.5 1.5l1.96 2.5H17V9.5h2.5zM6 18c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm2.22-3c-.55-.61-1.33-1-2.22-1s-1.67.39-2.22 1H3V6h12v9H8.22zM18 18c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z" />
@@ -54,6 +56,7 @@ export default {
         <template
             v-else-if="speedProfileId === 'CYCLING'"
         ><path
+            class="routing-speed-profile-icon-CYCLING"
             d="M0 0h24v24H0V0z"
             fill="none"
         /><path d="M15.5 5.5c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zM5 12c-2.8 0-5 2.2-5 5s2.2 5 5 5 5-2.2 5-5-2.2-5-5-5zm0 8.5c-1.9 0-3.5-1.6-3.5-3.5s1.6-3.5 3.5-3.5 3.5 1.6 3.5 3.5-1.6 3.5-3.5 3.5zm5.8-10l2.4-2.4.8.8c1.3 1.3 3 2.1 5.1 2.1V9c-1.5 0-2.7-.6-3.6-1.5l-1.9-1.9c-.5-.4-1-.6-1.6-.6s-1.1.2-1.4.6L7.8 8.4c-.4.4-.6.9-.6 1.4 0 .6.2 1.1.6 1.4L11 14v5h2v-6.2l-2.2-2.3zM19 12c-2.8 0-5 2.2-5 5s2.2 5 5 5 5-2.2 5-5-2.2-5-5-5zm0 8.5c-1.9 0-3.5-1.6-3.5-3.5s1.6-3.5 3.5-3.5 3.5 1.6 3.5 3.5-1.6 3.5-3.5 3.5z" /></template>
@@ -61,6 +64,7 @@ export default {
         <template
             v-else-if="speedProfileId === 'FOOT'"
         ><path
+            class="routing-speed-profile-icon-FOOT"
             d="M0 0h24v24H0V0z"
             fill="none"
         /><path d="M13.5 5.5c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zM9.8 8.9L7 23h2.1l1.8-8 2.1 2v6h2v-7.5l-2.1-2 .6-3C14.8 12 16.8 13 19 13v-2c-1.9 0-3.5-1-4.3-2.4l-1-1.6c-.56-.89-1.68-1.25-2.65-.84L6 8.3V13h2V9.6l1.8-.7" /></template>
@@ -68,6 +72,7 @@ export default {
         <template
             v-else-if="speedProfileId === 'WHEELCHAIR'"
         ><path
+            class="routing-speed-profile-icon-WHEELCHAIR"
             d="M0 0h24v24H0V0z"
             fill="none"
         /><circle
