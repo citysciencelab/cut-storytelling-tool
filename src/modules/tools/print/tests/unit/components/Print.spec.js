d600582456462007f878a4c3d7e775834873e8a5
Micha Pikora <Micha.Pikora@gv.hamburg.de>
2021-10-14 14:51:06 +0200

    BG-1613 additional unit tests for Print.vue

--- src/modules/tools/print/tests/unit/components/Print.spec.js
+++ src/modules/tools/print/tests/unit/components/Print.spec.js
@@ -26,7 +26,8 @@ describe("tools/Print/components/Print.vue", () => {
         mockGfiGetters = {
             currentFeature: () => sinon.stub()
         };
-    let store;
+    let store,
+        wrapper;
 
     beforeEach(() => {
         store = new Vuex.Store({
@@ -55,55 +56,61 @@ describe("tools/Print/components/Print.vue", () => {
         });
 
         store.commit("Tools/Print/setActive", true);
+
+        wrapper = mount(PrintComponent, {store, localVue});
     });
 
-
-    it("should have an existing title", () => {
-        const wrapper = mount(PrintComponent, {
-            store,
-            localVue
+    describe("template", () => {
+        it("should have an existing title", () => {
+            expect(wrapper.find("#printToolNew")).to.exist;
         });
 
-        expect(wrapper.find("#printToolNew")).to.exist;
-    });
-
-    it("should have a close button", () => {
-        const wrapper = mount(PrintComponent, {
-            store,
-            localVue
+        it("should have a close button", () => {
+            expect(wrapper.find(".glyphicon-remove")).to.exist;
         });
 
-        expect(wrapper.find(".glyphicon-remove")).to.exist;
+        it("should emitted close event if button is clicked", async () => {
+            const button = wrapper.find(".glyphicon-remove");
+
+            expect(button).to.exist;
+
+            button.trigger("click");
+            expect(wrapper.emitted()).to.have.property("close");
+            expect(wrapper.emitted().close).to.have.lengthOf(1);
+        });
+
+        it("should have a dropdown for layouts", () => {
+            expect(wrapper.find("#printLayout").exists()).to.be.true;
+        });
+        it("should have a dropdown for formats", () => {
+            expect(wrapper.find("#printFormat").exists()).to.be.true;
+        });
+        it("should have a dropdown for scales", () => {
+            expect(wrapper.find("#printScale").exists()).to.be.true;
+        });
     });
 
-    it("should emitted close event if button is clicked", async () => {
-        const wrapper = mount(PrintComponent, {
-                store,
-                localVue
-            }),
-            button = wrapper.find(".glyphicon-remove");
-
-        expect(button).to.exist;
-
-        await button.trigger("click");
-        expect(wrapper.emitted()).to.have.property("close");
-        expect(wrapper.emitted().close).to.have.lengthOf(1);
+    describe("returnScale", () => {
+        it("should return an empty string if anything but a number is given", () => {
+            expect(wrapper.vm.returnScale(undefined)).to.be.a("string").and.to.be.empty;
+            expect(wrapper.vm.returnScale(null)).to.be.a("string").and.to.be.empty;
+            expect(wrapper.vm.returnScale("string")).to.be.a("string").and.to.be.empty;
+            expect(wrapper.vm.returnScale(true)).to.be.a("string").and.to.be.empty;
+            expect(wrapper.vm.returnScale(false)).to.be.a("string").and.to.be.empty;
+            expect(wrapper.vm.returnScale([])).to.be.a("string").and.to.be.empty;
+            expect(wrapper.vm.returnScale({})).to.be.a("string").and.to.be.empty;
+        });
+        it("should return the given scale untouched if any number below 10.000 is given", () => {
+            expect(wrapper.vm.returnScale(9999)).to.equal("9999");
+            expect(wrapper.vm.returnScale(1)).to.equal("1");
+            expect(wrapper.vm.returnScale(0)).to.equal("0");
+            expect(wrapper.vm.returnScale(-1)).to.equal("-1");
+            expect(wrapper.vm.returnScale(-999999)).to.equal("-999999");
+        });
+        it("should return the given scale with spaces as thousands separators if any number above 9.999 is given", () => {
+            expect(wrapper.vm.returnScale(10000)).to.equal("10 000");
+            expect(wrapper.vm.returnScale(999999)).to.equal("999 999");
+            expect(wrapper.vm.returnScale(1000000)).to.equal("1 000 000");
+        });
     });
-
-    it("should check if dropdown for layouts exists", () => {
-        const wrapper = mount(PrintComponent, {store, localVue});
-
-        expect(wrapper.find("#printLayout").exists()).to.be.false;
-    });
-    it("should check if dropdown for formats exists", () => {
-        const wrapper = mount(PrintComponent, {store, localVue});
-
-        expect(wrapper.find("#printFormat").exists()).to.be.false;
-    });
-    it("should check if dropdown for scales exists", () => {
-        const wrapper = mount(PrintComponent, {store, localVue});
-
-        expect(wrapper.find("#printScale").exists()).to.be.false;
-    });
-
 });
