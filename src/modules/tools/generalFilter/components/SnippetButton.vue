9c6936ae76fe643aee41d708c2a98638963a92ac
cyros <mohammadreza.mossadegh@gv.hamburg.de>
2021-11-29 19:02:23 +0100

    G31DEV1-448-develope-progressbar - add progressbar

--- src/modules/tools/generalFilter/components/SnippetButton.vue
+++ src/modules/tools/generalFilter/components/SnippetButton.vue
@@ -1,15 +1,45 @@
 <script>
+import ProgressBar from "./ProgressBar.vue";
+
 export default {
-    name: "SnippetButton"
+    name: "SnippetButton",
+    components: {
+        ProgressBar
+    },
+    data () {
+        return {
+            delay: 100,
+            showProgress: false,
+            progressBarKey: 0
+        };
+    },
+    methods: {
+        runFilter () {
+            this.showProgress = true;
+            this.progressBarKey += 1;
+        }
+    }
 };
 </script>
 
 <template>
     <div>
-        <input
-            type="button"
-            :value="$t('button.confirm')"
+        <div>
+            <input
+                type="button"
+                :value="$t('button.confirm')"
+                @click="runFilter"
+            >
+        </div>
+        <div
+            v-if="showProgress"
         >
+            <br>
+            <ProgressBar
+                :key="progressBarKey"
+                :delay="delay"
+            />
+        </div>
     </div>
 </template>
 
