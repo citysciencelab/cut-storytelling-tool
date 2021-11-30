397ccb3d4bab413ed7f5202d579ec3b0a12c0ff9
sven_beyel <sven.beyel@gv.hamburg.de>
2021-11-30 14:25:37 +0100

    add remove unit tests in compareFeatures and update description in part of describe
    BG-2037

--- src/modules/tools/gfi/components/favoriteIcons/tests/unit/components/CompareFeaturesIcon.spec.js
+++ src/modules/tools/gfi/components/favoriteIcons/tests/unit/components/CompareFeaturesIcon.spec.js
@@ -6,36 +6,100 @@ import CompareFeatureIcon from "../../../components/CompareFeatureIcon.vue";
 config.mocks.$t = key => key;
 
 describe("src/modules/tools/gfi/favoriteIcons/components/CompareFeatureIcon.vue", () => {
-    let wrapper;
+    describe("featureIsOnCompareList = false and componentExists = true", () => {
+        let wrapper;
 
-    beforeEach(() => {
-        wrapper = shallowMount(CompareFeatureIcon, {
-            propsData: {
-                feature: {
-                    getId: () => "feature1",
-                    getLayerId: () => "1234",
-                    getTitle: () => "TestTitle",
-                    getAttributesToShow: () => "TestAttributes",
-                    getMappedProperties: () => "TestProperties"
+        beforeEach(() => {
+            wrapper = shallowMount(CompareFeatureIcon, {
+                propsData: {
+                    feature: {
+                        getId: () => "feature1",
+                        getLayerId: () => "1234",
+                        getTitle: () => "TestTitle",
+                        getAttributesToShow: () => "TestAttributes",
+                        getMappedProperties: () => "TestProperties"
+                    }
+                },
+                methods: {
+                    componentExists: () => sinon.fake.returns(true)
+                },
+                computed: {
+                    featureIsOnCompareList: sinon.fake.returns(false),
+                    mapMode: sinon.fake.returns("2D")
                 }
-            },
-            methods: {
-                componentExists: () => sinon.fake.returns(true)
-            },
-            computed: {
-                featureIsOnCompareList: sinon.fake.returns(false),
-                mapMode: sinon.fake.returns("2D")
-            }
+            });
+        });
+        afterEach(sinon.restore);
+
+        it("should draw a star if the compareFeatures is configured", () => {
+            expect(wrapper.find("span").exists()).to.be.true;
+        });
+        it("should render empty star button if feature is already on compare list", () => {
+            expect(wrapper.find("span").classes("glyphicon-star-empty")).to.be.true;
+            expect(wrapper.find("span").classes("glyphicon-star")).to.be.false;
+            expect(wrapper.find("span").attributes().title).equals("modules.tools.gfi.favoriteIcons.compareFeatureIcon.toCompareList");
         });
     });
-    afterEach(sinon.restore);
 
-    it("should draw a star if the compareFeatures is configured", () => {
-        expect(wrapper.find("span").exists()).to.be.true;
+    describe("featureIsOnCompareList = true  and componentExists = true", () => {
+        let wrapper;
+
+        beforeEach(() => {
+            wrapper = shallowMount(CompareFeatureIcon, {
+                propsData: {
+                    feature: {
+                        getId: () => "feature1",
+                        getLayerId: () => "1234",
+                        getTitle: () => "TestTitle",
+                        getAttributesToShow: () => "TestAttributes",
+                        getMappedProperties: () => "TestProperties"
+                    }
+                },
+                methods: {
+                    componentExists: () => true
+                },
+                computed: {
+                    featureIsOnCompareList: sinon.fake.returns(true),
+                    mapMode: sinon.fake.returns("2D")
+                }
+            });
+        });
+        afterEach(sinon.restore);
+
+        it("should render star button if feature is already on compare list", () => {
+            expect(wrapper.find("span").classes("glyphicon-star-empty")).to.be.false;
+            expect(wrapper.find("span").classes("glyphicon-star")).to.be.true;
+            expect(wrapper.find("span").attributes().title).equals("modules.tools.gfi.favoriteIcons.compareFeatureIcon.fromCompareList");
+        });
     });
-    it("should render empty star button if feature is already on compare list", () => {
-        expect(wrapper.find("span").classes("glyphicon-star-empty")).to.be.true;
-        expect(wrapper.find("span").classes("glyphicon-star")).to.be.false;
-        expect(wrapper.find("span").attributes().title).equals("modules.tools.gfi.favoriteIcons.compareFeatureIcon.toCompareList");
+
+    describe("featureIsOnCompareList = true  and componentExists = false", () => {
+        let wrapper;
+
+        beforeEach(() => {
+            wrapper = shallowMount(CompareFeatureIcon, {
+                propsData: {
+                    feature: {
+                        getId: () => "feature1",
+                        getLayerId: () => "1234",
+                        getTitle: () => "TestTitle",
+                        getAttributesToShow: () => "TestAttributes",
+                        getMappedProperties: () => "TestProperties"
+                    }
+                },
+                methods: {
+                    componentExists: () => false
+                },
+                computed: {
+                    featureIsOnCompareList: sinon.fake.returns(false),
+                    mapMode: sinon.fake.returns("2D")
+                }
+            });
+        });
+        afterEach(sinon.restore);
+
+        it("should not draw a star if compareFeatures is not configured", () => {
+            expect(wrapper.find("span").exists()).to.be.false;
+        });
     });
 });
