a6cd31d77ef7056d912dc0e942dec117df859c58
Tobias Erdrich <te@sinusquadrat.com>
2021-12-21 12:41:42 +0100

    Updated checkbox and radio classes to BT5

--- src/modules/tools/coordToolkit/components/CoordToolkit.vue
+++ src/modules/tools/coordToolkit/components/CoordToolkit.vue
@@ -430,6 +430,7 @@ export default {
                                 id="supplyCoordRadio"
                                 type="radio"
                                 name="mode"
+                                class="form-check-input"
                                 :checked="isSupplyCoordChecked()"
                                 :disabled="isSupplyCoordDisabled()"
                                 @click="changeMode('supply')"
@@ -437,7 +438,7 @@ export default {
                             <label
                                 for="supplyCoordRadio"
                                 :title="isSupplyCoordDisabled()? $t('modules.tools.coordToolkit.disabledTooltip'): ''"
-                                :class="{ 'form-label': true, 'enabled': isEnabled('supply') }"
+                                :class="{ 'form-check-label': true, 'enabled': isEnabled('supply') }"
                                 @click="changeMode('supply')"
                                 @keydown.enter="changeMode('supply')"
                             >{{ $t("modules.tools.coordToolkit.supply") }}</label>
@@ -447,12 +448,13 @@ export default {
                                 id="searchByCoordRadio"
                                 type="radio"
                                 name="mode"
+                                class="form-check-input"
                                 :checked="!isSupplyCoordChecked()"
                                 @click="changeMode('search')"
                             >
                             <label
                                 for="searchByCoordRadio"
-                                :class="{'form-label': true, 'enabled': isEnabled('search') }"
+                                :class="{'form-check-label': true, 'enabled': isEnabled('search') }"
                                 @click="changeMode('search')"
                                 @keydown.enter="changeMode('search')"
                             >{{ $t("modules.tools.coordToolkit.search") }}</label>
