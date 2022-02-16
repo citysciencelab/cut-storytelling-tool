91816a157fd6e19356d0c4634d841aeeab0862cd
DargeFi <finn-rasmus.darge@dataport.de>
2022-02-16 13:01:24 +0100

    add test for feature list rendering

--- src/modules/tools/featureLister/tests/unit/components/FeatureLister.spec.js
+++ src/modules/tools/featureLister/tests/unit/components/FeatureLister.spec.js
@@ -4,7 +4,7 @@ import sinon from "sinon";
 import {config, shallowMount, createLocalVue} from "@vue/test-utils";
 import FeatureListerComponent from "../../../components/FeatureLister.vue";
 import FeatureLister from "../../../store/indexFeatureLister";
-// import VectorLayer from "ol/layer/Vector.js";
+import VectorLayer from "ol/layer/Vector.js";
 
 const localVue = createLocalVue();
 
@@ -30,15 +30,14 @@ describe("src/modules/tools/featureLister/components/FeatureLister.vue", () => {
                     }
                 }
             }
-        },
-        switchTabToSpy = sinon.spy(),
-        addMouseEventsSpy = sinon.spy();
+        };
     let store,
         wrapper;
 
     beforeEach(() => {
         FeatureLister.actions.switchTabTo = sinon.spy();
         FeatureLister.actions.addMouseEvents = sinon.spy();
+        FeatureLister.getters.headers = () => [{key: "name", value: "Name"}];
 
         store = new Vuex.Store({
             namespaced: true,
@@ -56,13 +55,6 @@ describe("src/modules/tools/featureLister/components/FeatureLister.vue", () => {
             },
             state: {
                 configJson: mockConfigJson
-            },
-            actions: {
-                switchTabTo: switchTabToSpy,
-                addMouseEvents: addMouseEventsSpy
-            },
-            getters: {
-                getHeaders: () => ["name", "id"]
             }
         });
     });
@@ -71,36 +63,23 @@ describe("src/modules/tools/featureLister/components/FeatureLister.vue", () => {
         store.commit("Tools/FeatureLister/setActive", true);
         store.commit("Tools/FeatureLister/setLayerListView", true);
         wrapper = shallowMount(FeatureListerComponent, {store, localVue});
-
         expect(wrapper.find("#featurelist-themes").exists()).to.be.true;
     });
 
-    // it("renders list of layer features", () => {
-    //     const layer1 = new VectorLayer({
-    //             name: "ersterLayer", id: "123", features: [{getAttributesToShow: () => "TestAttributes"}], geometryType: "Point", values_: [1, 2]
-    //         }),
-    //         layer2 = new VectorLayer({
-    //             name: "ersterLayer", id: "123", features: [{getAttributesToShow: () => "TestAttributes"}], geometryType: "Point", values_: [1, 2]
-    //         }),
-    //         layerList = [layer1, layer2],
-    //         layer = {name: "ersterLayer", id: "123", features: [{values_: {features: [1, 2]}}], geometryType: "Point"};
+    it("renders list of layer features", () => {
+        const layer1 = new VectorLayer({
+                name: "ersterLayer", id: "123", features: [{getAttributesToShow: () => [{key: "name", value: "Name"}]}], geometryType: "Point", values_: [1, 2]
+            }),
+            layer2 = new VectorLayer({
+                name: "ersterLayer", id: "123", features: [{getAttributesToShow: () => [{key: "name", value: "Name"}]}], geometryType: "Point", values_: [1, 2]
+            }),
+            layerList = [layer1, layer2],
+            layer = {name: "ersterLayer", id: "123", features: [{values_: {features: [1, 2]}}], geometryType: "Point"};
 
-    //     store.commit("Tools/FeatureLister/setVisibleLayers", layerList);
-    //     store.dispatch("Tools/FeatureLister/switchToList", layer);
-    //     wrapper = shallowMount(FeatureListerComponent, {store, localVue});
+        store.commit("Tools/FeatureLister/setVisibleLayers", layerList);
+        store.dispatch("Tools/FeatureLister/switchToList", layer);
+        wrapper = shallowMount(FeatureListerComponent, {store, localVue});
 
-    //     expect(wrapper.find("#featurelist-list").exists()).to.be.true;
-    // });
-
-    // describe("CompareFeatures.vue methods", () => {
-    //     it("close sets active to false", async () => {
-    //         wrapper = shallowMount(CompareFeaturesComponent, {store, localVue});
-
-    //         wrapper.vm.close();
-    //         await wrapper.vm.$nextTick();
-
-    //         expect(store.state.Tools.CompareFeatures.active).to.be.false;
-    //         expect(wrapper.find("#tool-compareFeatures").exists()).to.be.false;
-    //     });
-    // });
+        expect(wrapper.find("#featurelist-list").exists()).to.be.true;
+    });
 });
