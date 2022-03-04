7d65084f545497a93558b7cc545c5c1227754a62
DargeFi <finn-rasmus.darge@dataport.de>
2022-03-04 06:47:42 +0100

    Implement hover behavior for features

--- src/modules/mouseHover/components/MouseHover.vue
+++ src/modules/mouseHover/components/MouseHover.vue
@@ -1,11 +1,16 @@
 <script>
-import {mapActions} from "vuex";
+import {mapActions, mapGetters} from "vuex";
 import actions from "../store/actionsMouseHover";
 
 export default {
     name: "MouseHover",
+    computed: {
+        ...mapGetters("Map", ["ol2DMap"])
+    },
     mounted () {
-        this.initialize();
+        this.$nextTick(function () {
+            this.initialize(this.ol2DMap);
+        });
     },
     methods: {
         ...mapActions("MouseHover", Object.keys(actions))
--- src/modules/mouseHover/store/actionsMouseHover.js
+++ src/modules/mouseHover/store/actionsMouseHover.js
@@ -1,13 +1,43 @@
+import {createGfiFeature} from "../../../api/gfi/getWmsFeaturesByMimeType";
 
 export default {
     /**
      * Sets the config-params of this MouseHover into state.
+     * @param {module:ol/Map} map map object
      * @returns {void}
      */
-    initialize: ({commit}) => {
+    initialize ({commit}, map) {
         const {numFeaturesToShow, infoText} = Config.mouseHover;
+        let featuresAtPixel = [];
 
-        commit("setNumFeaturesToShow", numFeaturesToShow);
-        commit("setInfoText", infoText);
+        if (numFeaturesToShow) {
+            commit("setNumFeaturesToShow", numFeaturesToShow);
+        }
+        if (infoText) {
+            commit("setInfoText", infoText);
+        }
+        map.on("pointermove", (evt) => {
+            featuresAtPixel = [];
+            map.forEachFeatureAtPixel(evt.pixel, (feature, layer) => {
+                if (layer?.getVisible() && layer?.get("gfiAttributes") && layer?.get("gfiAttributes") !== "ignore") {
+                    if (feature.getProperties().features) {
+                        feature.get("features").forEach(function (clusteredFeature) {
+                            featuresAtPixel.push(createGfiFeature(
+                                layer,
+                                "",
+                                clusteredFeature
+                            ));
+                        });
+                    }
+                    else {
+                        featuresAtPixel.push(createGfiFeature(
+                            layer,
+                            "",
+                            feature
+                        ));
+                    }
+                }
+            });
+        });
     }
 };
