b2baa4a99335ea6387f5e38b7c1e55d930c364aa
cyros <mohammadreza.mossadegh@gv.hamburg.de>
2022-03-29 10:29:46 +0200

    G31DEV1-604-oaf-filterung-getminmax-uniq
    filterGeneral: implementation of get unique value and get min max
    functions for oaf.

--- src/modules/tools/filterGeneral/interfaces/filter.api.js
+++ src/modules/tools/filterGeneral/interfaces/filter.api.js
@@ -83,7 +83,7 @@ export default class FilterApi {
                     extern,
                     layerId,
                     url: layerModel.get("url"),
-                    collection: layerModel.get("collection") || layerModel.get("featureType"),
+                    collection: layerModel.get("collection"),
                     jsonAcceptHeader: layerModel.get("jsonAcceptHeader"),
                     namespace: featureNS
                 };
