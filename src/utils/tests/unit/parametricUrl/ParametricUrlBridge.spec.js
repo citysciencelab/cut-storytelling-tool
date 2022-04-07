88e17c5fad2a82fb66cd7b1b27d0cedda8a13c53
fmlgv <friedrich.mueller@gv.hamburg.de>
2022-04-07 16:13:57 +0200

    fix ParametricUrlBridge unit test

--- src/utils/tests/unit/parametricUrl/ParametricUrlBridge.spec.js
+++ src/utils/tests/unit/parametricUrl/ParametricUrlBridge.spec.js
@@ -187,7 +187,7 @@ describe("src/utils/parametricUrl/ParametricUrlBridge.js", () => {
         });
         it("test url param key 'Map/zoomToExtent'", () => {
             store.state.urlParams.projection = undefined;
-            store.state.Map.projection = undefined;
+            store.state.Maps.projection = undefined;
             const radioTrigger = sinon.spy(Radio, "trigger"),
                 key = "Map/zoomToExtent",
                 value = [510000, 5850000, 625000, 6000000],
