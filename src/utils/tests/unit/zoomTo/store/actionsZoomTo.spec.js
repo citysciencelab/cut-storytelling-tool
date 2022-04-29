74bf07603fb1b70d45870bd93c4afece68ec74cf
Pascal Röhling <pascal.roehling@dataport.de>
2022-04-29 14:26:14 +0200

    Adjust and add tests for new configuration parameter addFeatures

--- src/utils/tests/unit/zoomTo/store/actionsZoomTo.spec.js
+++ src/utils/tests/unit/zoomTo/store/actionsZoomTo.spec.js
@@ -94,7 +94,7 @@ describe("src/utils/zoomTo/store/actionsZoomTo.js", () => {
             expect(consoleErrorSpy.firstCall.args.length).to.equal(2);
             expect(consoleErrorSpy.firstCall.args).to.eql(["zoomTo: An error occurred while trying to fetch features from the given service.", "Custom testing error!"]);
         });
-        it("should fetch features and call respective vuex store functions if a correct (but deprecated) configuration and url parameter match for zoomToFeature is prov", async () => {
+        it("should fetch features and call respective vuex store functions if a correct (but deprecated) configuration and url parameter match for zoomToFeature is provided", async () => {
             getters.deprecatedParameters = true;
             getters.config.zoomToFeature = {
                 wfsId: id,
@@ -113,6 +113,30 @@ describe("src/utils/zoomTo/store/actionsZoomTo.js", () => {
             expect(commit.firstCall.args[1] instanceof VectorLayer).to.be.true;
             expect(commit.firstCall.args[1].getSource().getFeatures().length).to.equal(2);
             expect(commit.firstCall.args[2]).to.eql({root: true});
+            expect(dispatch.calledOnce).to.be.true;
+            expect(dispatch.firstCall.args.length).to.equal(3);
+            expect(dispatch.firstCall.args[0]).to.equal("Map/zoomTo");
+            expect(dispatch.firstCall.args[1] instanceof Object).to.be.true;
+            expect(Object.prototype.hasOwnProperty.call(dispatch.firstCall.args[1], "geometryOrExtent")).to.be.true;
+            expect(dispatch.firstCall.args[1].geometryOrExtent.length).to.equal(4);
+            expect(dispatch.firstCall.args[1].geometryOrExtent.every(val => typeof val === "number")).to.be.true;
+            expect(dispatch.firstCall.args[2]).to.eql({root: true});
+        });
+        it("should fetch features and call respective vuex store functions if a correct (but deprecated) configuration and url parameter match for zoomToFeature is provided; not adding the features to the map as a new layer through config parameter", async () => {
+            getters.deprecatedParameters = true;
+            getters.config.zoomToFeature = {
+                wfsId: id,
+                attribute: "flaechenid",
+                styleId: "stylish",
+                addFeatures: false
+            };
+            state.zoomToFeatureId = [18, 26];
+            await actions.zoomToFeatures({state, getters, commit, dispatch});
+
+            expect(consoleWarnSpy.calledOnce).to.be.true;
+            expect(consoleWarnSpy.firstCall.args.length).to.equal(1);
+            expect(consoleWarnSpy.firstCall.args[0]).to.equal("zoomTo: A deprecated configuration was found. Using it, until it gets removed...");
+            expect(commit.notCalled).to.be.true;
             expect(dispatch.calledOnce).to.be.true;
             expect(dispatch.firstCall.args.length).to.equal(3);
             expect(dispatch.firstCall.args[0]).to.equal("Map/zoomTo");
@@ -278,6 +302,34 @@ describe("src/utils/zoomTo/store/actionsZoomTo.js", () => {
             expect(requestSpy.secondCall.args.length).to.equal(3);
             expect(requestSpy.secondCall.args).to.eql(["StyleList", "returnModelById", "stylish"]);
         });
+        it("should zoom to the feature extent but not add the features for one config of zoomToFeatureId with addFeatures set to false", async () => {
+            getters.config = [{
+                id: "zoomToFeatureId",
+                layerId: id,
+                property: "flaechenid",
+                styleId: "stylish",
+                addFeatures: false
+            }];
+            state.zoomToFeatureId = [18, 26];
+            await actions.zoomToFeatures({state, getters, commit, dispatch});
+
+            expect(consoleWarnSpy.notCalled).to.be.true;
+            expect(consoleErrorSpy.notCalled).to.be.true;
+            expect(dispatch.calledOnce).to.be.true;
+            expect(dispatch.firstCall.args.length).to.equal(3);
+            expect(dispatch.firstCall.args[0]).to.equal("Map/zoomTo");
+            expect(dispatch.firstCall.args[1] instanceof Object).to.be.true;
+            expect(Object.prototype.hasOwnProperty.call(dispatch.firstCall.args[1], "geometryOrExtent")).to.be.true;
+            expect(dispatch.firstCall.args[1].geometryOrExtent.length).to.equal(4);
+            expect(dispatch.firstCall.args[1].geometryOrExtent.every(val => typeof val === "number")).to.be.true;
+            expect(dispatch.firstCall.args[2]).to.eql({root: true});
+            expect(commit.notCalled).to.be.true;
+            expect(requestSpy.calledTwice).to.be.true;
+            expect(requestSpy.firstCall.args.length).to.equal(3);
+            expect(requestSpy.firstCall.args).to.eql(["StyleList", "returnModelById", "stylish"]);
+            expect(requestSpy.secondCall.args.length).to.equal(3);
+            expect(requestSpy.secondCall.args).to.eql(["StyleList", "returnModelById", "stylish"]);
+        });
         it("should add features to the map for one config of zoomToGeometry", async () => {
             getters.config = [{
                 id: "zoomToGeometry",
@@ -305,6 +357,30 @@ describe("src/utils/zoomTo/store/actionsZoomTo.js", () => {
             expect(commit.firstCall.args[1] instanceof VectorLayer).to.be.true;
             expect(commit.firstCall.args[1].getSource().getFeatures().length).to.equal(1);
             expect(commit.firstCall.args[2]).to.eql({root: true});
+        });
+        it("should zoom to the feature extent but not add the features for one config of zoomToGeometry with addFeatures set to false", async () => {
+            getters.config = [{
+                id: "zoomToGeometry",
+                layerId: id,
+                property: "flaechenid",
+                allowedValues: [18, 26, 42],
+                addFeatures: false
+            }];
+            state.zoomToGeometry = "18,25";
+            await actions.zoomToFeatures({state, getters, commit, dispatch});
+
+            expect(consoleWarnSpy.notCalled).to.be.true;
+            expect(consoleErrorSpy.notCalled).to.be.true;
+            expect(requestSpy.notCalled).to.be.true;
+            expect(dispatch.calledOnce).to.be.true;
+            expect(dispatch.firstCall.args.length).to.equal(3);
+            expect(dispatch.firstCall.args[0]).to.equal("Map/zoomTo");
+            expect(dispatch.firstCall.args[1] instanceof Object).to.be.true;
+            expect(Object.prototype.hasOwnProperty.call(dispatch.firstCall.args[1], "geometryOrExtent")).to.be.true;
+            expect(dispatch.firstCall.args[1].geometryOrExtent.length).to.equal(4);
+            expect(dispatch.firstCall.args[1].geometryOrExtent.every(val => typeof val === "number")).to.be.true;
+            expect(dispatch.firstCall.args[2]).to.eql({root: true});
+            expect(commit.notCalled).to.be.true;
         });
         it("should add features to the map for one config of zoomToFeatureId and one of zoomToGeometry", async () => {
             getters.config = [
@@ -335,12 +411,17 @@ describe("src/utils/zoomTo/store/actionsZoomTo.js", () => {
             expect(dispatch.firstCall.args[1].geometryOrExtent.length).to.equal(4);
             expect(dispatch.firstCall.args[1].geometryOrExtent.every(val => typeof val === "number")).to.be.true;
             expect(dispatch.firstCall.args[2]).to.eql({root: true});
-            expect(commit.calledOnce).to.be.true;
+            expect(commit.calledTwice).to.be.true;
             expect(commit.firstCall.args.length).to.equal(3);
             expect(commit.firstCall.args[0]).to.equal("Map/addLayerToMap");
             expect(commit.firstCall.args[1] instanceof VectorLayer).to.be.true;
-            expect(commit.firstCall.args[1].getSource().getFeatures().length).to.equal(2);
+            expect(commit.firstCall.args[1].getSource().getFeatures().length).to.equal(1);
             expect(commit.firstCall.args[2]).to.eql({root: true});
+            expect(commit.secondCall.args.length).to.equal(3);
+            expect(commit.secondCall.args[0]).to.equal("Map/addLayerToMap");
+            expect(commit.secondCall.args[1] instanceof VectorLayer).to.be.true;
+            expect(commit.secondCall.args[1].getSource().getFeatures().length).to.equal(1);
+            expect(commit.secondCall.args[2]).to.eql({root: true});
             expect(requestSpy.calledTwice).to.be.true;
             expect(requestSpy.firstCall.args.length).to.equal(3);
             expect(requestSpy.firstCall.args).to.eql(["StyleList", "returnModelById", "stylish"]);
