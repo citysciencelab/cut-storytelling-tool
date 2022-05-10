9032ef46b0e13cecc89a77f2fa6d666a63f779ab
Melanie M. Klamp <melaniemichaela.klamp@dataport.de>
2022-05-10 20:19:32 +0200

    Error display when using default tree

--- src/utils/featureViaURL/index.js
+++ src/utils/featureViaURL/index.js
@@ -138,6 +138,9 @@ export default function ({layers, epsg, zoomTo}) {
             if (parentId !== undefined) {
                 addGeoJSON(layers[pos].name, layers[pos].id, geoJSON, layers[pos].styleId, parentId, gfiAttributes);
                 Radio.trigger("Util", "refreshTree");
+            } else {
+                Radio.trigger("Alert", "alert", i18next.t("common:modules.featureViaURL.messages.defaultTreeNotSupported"));
+                return;
             }
             if (typeof zoomTo !== "undefined" && (zoomTo === layerId || zoomTo.indexOf(layerId) !== -1)) {
                 Radio.trigger("Map", "zoomToFilteredFeatures", getFeatureIds(layerId), layerId);
