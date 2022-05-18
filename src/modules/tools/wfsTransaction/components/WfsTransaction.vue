034637b392117f895518d485ea1d95b771f38259
Pascal Röhling <pascal.roehling@dataport.de>
2022-05-18 13:20:41 +0200

    Add basic tool template for WfsTransaction

--- src/modules/tools/wfsTransaction/components/WfsTransaction.vue
+++ src/modules/tools/wfsTransaction/components/WfsTransaction.vue
@@ -1,10 +1,23 @@
 <template>
-    <div />
+    <ToolTemplate
+        :title="$t(name)"
+        :icon="icon"
+        :active="active"
+        :render-to-window="renderToWindow"
+        :resizable-window="resizableWindow"
+        :initial-width="initialWidth"
+        :initial-width-mobile="initialWidthMobile"
+        :deactivate-gfi="deactivateGFI"
+    >
+        <template #toolBody />
+    </ToolTemplate>
 </template>
 
 <script>
+import ToolTemplate from "../../ToolTemplate.vue";
 export default {
-    name: "WfsTransaction"
+    name: "WfsTransaction",
+    components: {ToolTemplate}
 };
 </script>
 
