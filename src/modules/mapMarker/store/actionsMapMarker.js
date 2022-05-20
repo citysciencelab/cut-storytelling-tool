476d554d8aea04a7d66da46b813424a56443f4f5
fmlgv <friedrich.mueller@gv.hamburg.de>
2022-05-20 09:13:17 +0200

    rm Cesium specific getters from map

--- src/modules/mapMarker/store/actionsMapMarker.js
+++ src/modules/mapMarker/store/actionsMapMarker.js
@@ -1,6 +1,7 @@
 import {fetchFirstModuleConfig} from "../../../utils/fetchFirstModuleConfig";
 import Point from "ol/geom/Point.js";
 import Feature from "ol/Feature.js";
+import mapCollection from "../../../core/maps/mapCollection.js";
 
 /**
  * @const {String} configPaths an array of possible config locations. First one found will be used
