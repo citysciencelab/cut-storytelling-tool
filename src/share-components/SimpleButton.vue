fb5c01a4772d3d8c712634d0518d6f74a1f2d01f
Pascal Röhling <pascal.roehling@dataport.de>
2022-05-24 12:52:03 +0200

    Add a simple reusable button component

---
+++ src/share-components/SimpleButton.vue
@@ -0,0 +1,41 @@
+<template>
+    <button
+        class="btn btn-primary"
+        @click="interaction"
+    >
+        <span class="bootstrap-icon">
+            <i
+                v-if="icon !== null"
+                :class="icon"
+            />
+            {{ $t(caption) }}
+        </span>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "SimpleButton",
+    props: {
+        caption: {
+            type: String,
+            required: true
+        },
+        interaction: {
+            type: Function,
+            required: true
+        },
+        icon: {
+            type: String,
+            default: null,
+            required: false
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+    margin: 5px;
+}
+</style>
