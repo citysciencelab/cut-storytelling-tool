d4ff7775a50260103650b553f61ef98b0a0610fd
fmlgv <friedrich.mueller@gv.hamburg.de>
2022-06-01 14:04:00 +0200

    BG-2186: avoid urlisVisible after metadata window change

--- src/modules/layerInformation/components/LayerInformation.vue
+++ src/modules/layerInformation/components/LayerInformation.vue
@@ -329,6 +329,7 @@ export default {
                         </div>
                     </div>
                     <div
+                        v-if="showUrl"
                         id="url"
                         :show="isActiveTab('url')"
                         :class="getTabPaneClasses('url')"
