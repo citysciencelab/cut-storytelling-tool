2ef5bcefffc88654a70c8761055a83b9ada65dc7
herzogrh <rico.herzog@outlook.com>
2022-06-07 18:57:25 +0200

    db connection update

--- story-backend/controllers/controllers.js
+++ story-backend/controllers/controllers.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'me',
-  host: 'http://postgres-storytelling',
+  host: 'postgres-storytelling',
   database: 'stories',
   password: 'password',
   port: 5432,
