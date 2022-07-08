2b310085c2a17009b0677f6b9faf4fb0fd6bb81b
Till Degkwitz <smasho@web.de>
2022-07-08 14:24:43 +0200

    DELETE new creation addon

--- addons/storyCreationTool/store/gettersStoryCreationTool.js
+++
@@ -1,8 +0,0 @@
-import {generateSimpleGetters} from "../../../src/app-store/utils/generators";
-import storyCreationToolState from "./stateStoryCreationTool";
-
-const getters = {
-    ...generateSimpleGetters(storyCreationToolState)
-};
-
-export default getters;
