e2c2a0344bc644e8d13aa5de0df8dc9b518b1087
sven_beyel <sven.junker-beyel@gv.hamburg.de>
2022-07-22 12:49:00 +0200

    fix now only the visible layers are used to display the GFI
    In the built portal it came to the misbehavior, because the layerList from the map in the getters were not reloaded
    
    BG-3097

--- src/core/maps/store/actions/actionsMap.js
+++ src/core/maps/store/actions/actionsMap.js
@@ -12,6 +12,41 @@ import {getWmsFeaturesByMimeType} from "../../../../api/gfi/getWmsFeaturesByMime
 import getProxyUrl from "../../../../utils/getProxyUrl";
 import parseCesiumParameters from "../../../../utils/parseCesiumParameters";
 
+/**
+ * Gets all visible layers with children from group layers.
+ * @param {Object[]} visibleLayers The visible layers.
+ * @returns {Object[]} All visible layers.
+ */
+function getVisibleLayersWithGroupLayersChildren (visibleLayers) {
+    const visibleLayersWithGroupLayersChildren = [];
+
+    visibleLayers.forEach(layer => {
+        if (layer.get("layers") && typeof layer.get("layers").getArray === "function") {
+            layer.get("layers").getArray().forEach(childLayer => visibleLayersWithGroupLayersChildren.push(childLayer));
+        }
+        else {
+            visibleLayersWithGroupLayersChildren.push(layer);
+        }
+    });
+
+    return visibleLayersWithGroupLayersChildren;
+}
+
+/**
+ * Gets the visible wms layer at the current resolution.
+ * @param {Number} resolution The current resolution.
+ * @returns {Object[]} The visible wms layers.
+ */
+function getVisibleWmsLayersAtResolution (resolution) {
+    const visibleLayers = mapCollection.getMap("2D").getLayers().getArray().filter(layer => layer.getVisible()),
+        visibleLayersWithGroupLayersChildren = getVisibleLayersWithGroupLayersChildren(visibleLayers),
+        visibleLayersWms = visibleLayersWithGroupLayersChildren.filter(layer => {
+            return (layer.get("typ") === "WMS") && (resolution <= layer.get("maxResolution") && resolution >= layer.get("minResolution"));
+        });
+
+    return visibleLayersWms;
+}
+
 export default {
 
     /**
@@ -23,8 +58,8 @@ export default {
      * @returns {void}
      */
     collectGfiFeatures ({getters, commit, dispatch}) {
-        const {clickCoordinate, visibleWmsLayerListAtResolution, resolution, projection, gfiFeaturesAtPixel} = getters,
-            gfiWmsLayerList = visibleWmsLayerListAtResolution.filter(layer => {
+        const {clickCoordinate, resolution, projection, gfiFeaturesAtPixel} = getters,
+            gfiWmsLayerList = getVisibleWmsLayersAtResolution(resolution).filter(layer => {
                 return layer.get("gfiAttributes") !== "ignore";
             });
 
