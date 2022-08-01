cf0772ef296a67ec2ad9aa0285a8383a19d4326c
Feng Feng <feng.feng@gv.hamburg.de>
2022-08-01 15:39:33 +0200

    G31DEV1-911 Fix: getting the response data in loop in right way
    After the corejs is updated, the filter does not work any more.
    The problem is that we use forEach function for htmlCollection,
    but it does not work any more. In stead of forEach, the for loop
    will be used.

--- src/modules/tools/filter/interfaces/interface.wfs.extern.js
+++ src/modules/tools/filter/interfaces/interface.wfs.extern.js
@@ -610,7 +610,7 @@ export default class InterfaceWfsExtern {
         }
         const result = {};
 
-        responseXML.firstElementChild.children.forEach(element => {
+        for (const element of responseXML.firstElementChild.children) {
             let node = this.getNodeByTagname(element, typename);
 
             if (!node) {
@@ -621,7 +621,8 @@ export default class InterfaceWfsExtern {
             if (node) {
                 result[node.textContent] = true;
             }
-        });
+        }
+
         if (typeof onsuccess === "function") {
             onsuccess(Object.keys(result));
         }
