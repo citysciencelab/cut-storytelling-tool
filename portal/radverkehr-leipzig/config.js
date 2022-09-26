ea3572865d3bd81b57af112ce9e3281dfc359480
Till Degkwitz <smasho@web.de>
2022-09-26 13:14:17 +0200

    radverkehr leipzig test

---
+++ portal/radverkehr-leipzig/config.js
@@ -0,0 +1,28 @@
+const Config = {
+    addons: ["storyTellingTool"],
+    vuetify: "addons/storyTellingTool/vuetify",
+    storyConf: "./assets/story.json",
+    uiStyle: "table",
+    namedProjections: [
+        ['EPSG:25833',
+            '+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs']
+    ],
+    footer: {
+        urls: [
+            {
+                "bezeichnung": "common:modules.footer.designation",
+                "url": "https://geoinfo.hamburg.de/",
+                "alias": "Landesbetrieb Geoinformation und Vermessung",
+                "alias_mobil": "LGV"
+            }
+        ],
+        showVersion: true
+    },
+    layerConf: "./assets/services-internet.json",
+    scaleLine: true,
+};
+
+// conditional export to make config readable by e2e tests
+if (typeof module !== "undefined") {
+    module.exports = Config;
+}
